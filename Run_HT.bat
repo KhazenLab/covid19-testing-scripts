56aa71f6da5cd3766bede8929dec6542b2324558
halimt92 <48407395+halimt92@users.noreply.github.com>
2020-04-17 14:55:47 +0300

    fixup

--- Run_HT.bat
+++ Run_HT.bat
@@ -3,8 +3,8 @@
 
 py main.py l0-importbiominers "C:\Users\Halim\Desktop\covid page\Notion_key.txt" "C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\l0-notion_tables\multiple-biominers-gitrepo.csv"
 
-py main.py l1-importothers "C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\"
+py main.py l1-importothers C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\
 
-py main.py l2-mergetogether  "C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\"
+py main.py l2-mergetogether  C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\
 
-py main.py l3-generatearcdata "C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\"
+py main.py l3-generatearcdata C:\Users\Halim\Documents\GitHub\Covid19_Testing\covid19-testing-data\
